7afd4c4282a99a6afff9c30351101f40348d52b3
Mike Miller <github@mikeage.net>
2013-01-30 22:01:23 +0200

    Use the siblings_callback instead of parent()->children()
    update paginator

--- views/paginator.html.php
+++ views/paginator.html.php
@@ -70,12 +70,11 @@
       endif;
 
       $total_pages = $total;
-      if (isset($parent)):
-        $siblings = $parent->children();
-        for ($i = 1; $i <= $total; $i++):
-          $_pagelist[$i] = $siblings[$i-1]->url();
-        endfor;
-      endif;
+	  // Added in the latest git
+	  $siblings = $theme->siblings();
+	  for ($i = 1; $i <= $total; $i++):
+		  $_pagelist[$i] = $siblings[$i-1]->url();
+      endfor;
       break;
     default:
       $current_page = 1;
